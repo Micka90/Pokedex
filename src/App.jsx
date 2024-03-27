94230904774cb3759f829af2e5ffb4c38381ed0d
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-03-27 15:11:36 +0100

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import PokemonCard from "./components/PokemonCard.jsx"
+import PokemonCard from "./components/PokemonCard.jsx";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   return (
-    <div>
-      <PokemonCard />
-    </div>
+    <>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </>
   );
 }
 
 export default App;
-
-
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,9 @@
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard(props) {
+  const {pokemon} = props
+  
+
   return (
     <figure>
-      {pokemon.imgSrc
-        ? (<img></img>)
-        : <p>???</p>
-      }
       <img src={pokemon.imgSrc} />
       <figcaption>{pokemon.name}</figcaption>
     </figure>
@@ -13,15 +11,4 @@ function PokemonCard() {
 }
 
 export default PokemonCard;
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
