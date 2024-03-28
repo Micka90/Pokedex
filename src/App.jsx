cdd16dfeb9fa82df27ed5c6164291748b7e179d2
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-03-28 15:11:29 +0100

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import PokemonCard from "./components/PokemonCard.jsx";
+import "./App.css";
+import { useState } from "react";
+
 
 const pokemonList = [
-  {
+{
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
@@ -12,9 +30,15 @@ const pokemonList = [
 ];
 
 function App() {
+
+const [pokemonindex, setIndex] =useState (0)
+
   return (
     <>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonindex]} />
+
+      {pokemonindex > 0 ?<button onClick={() => setIndex (pokemonindex -1) }> Précendent</button> : null} 
+      <button onClick={ () => setIndex (pokemonindex + 1)}>Suivant</button>
     </>
   );
 }
